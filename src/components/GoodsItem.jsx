b721ad969a04658f1e292fa6b114e3ccc34d29d3
liubovbutorina <liubovbutorina7@gmail.com>
2023-11-24 01:16:20 +0300

    add style

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -22,7 +22,9 @@ function GoodsItem({ good, addToCart, order }) {
       <div className="card-reveal">
         <div className="card-title">
           <div className="card-name grey-text text-darken-4">{name}</div>
-          <i className="reveal material-icons right">close</i>
+          <i style={{ right: "-10px" }} className="reveal material-icons right">
+            close
+          </i>
         </div>
         <p className="card-description">{description}</p>
       </div>
